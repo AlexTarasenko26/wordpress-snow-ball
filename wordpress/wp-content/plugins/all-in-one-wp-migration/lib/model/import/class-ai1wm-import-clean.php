0f546911675c861f0b67f6080927fb8fe37e7fc3
AlexTarasenko26 <Aliaksei_Tarasenka@epam.com>
2024-06-02 22:54:29 +0200

    update plugin all in one migration to 7.82

--- wordpress/wp-content/plugins/all-in-one-wp-migration/lib/model/import/class-ai1wm-import-clean.php
+++ wordpress/wp-content/plugins/all-in-one-wp-migration/lib/model/import/class-ai1wm-import-clean.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (C) 2014-2020 ServMask Inc.
+ * Copyright (C) 2014-2023 ServMask Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,18 +30,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Ai1wm_Import_Clean {
 
 	public static function execute( $params ) {
-		global $wpdb;
-
 		// Get database client
-		if ( empty( $wpdb->use_mysqli ) ) {
-			$mysql = new Ai1wm_Database_Mysql( $wpdb );
-		} else {
-			$mysql = new Ai1wm_Database_Mysqli( $wpdb );
-		}
+		$db_client = Ai1wm_Database_Utility::create_client();
 
 		// Flush mainsite tables
-		$mysql->add_table_prefix_filter( ai1wm_table_prefix( 'mainsite' ) );
-		$mysql->flush();
+		$db_client->add_table_prefix_filter( ai1wm_table_prefix( 'mainsite' ) );
+		$db_client->flush();
 
 		// Delete storage files
 		Ai1wm_Directory::delete( ai1wm_storage_path( $params ) );
